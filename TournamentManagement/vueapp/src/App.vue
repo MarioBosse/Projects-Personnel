73c88a215aebffb55cac2ac30839286e1fa4c967
Mario Bossé <mariobosse007@gmail.com>
2023-05-19 08:36:08 -0400

    Lancement
    Création du projet
    2 applications créés
    - BackEnd C#
    - FrontEnd Vue & Bootstrap
    - Menu préliminaire
    - Base pour afficher les photos dans une grille

---
+++ TournamentManagement/vueapp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Header></Header>
+  <HelloWorld msg="Bienvenue de notre application de gestion de tournois" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+//import { appendFile } from 'fs';
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
